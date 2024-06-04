4875d8f47fa473a820433ba8c784b9516a273351
YashSoPro <105553471+YashSoPro@users.noreply.github.com>
2024-06-04 18:35:15 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     // GitHub repositories fetching
-    fetch('https://api.github.com/users/your-github-username/repos')
+    fetch('https://api.github.com/users/YashSoPro/repos')
         .then(response => response.json())
         .then(data => {
             const projectList = document.getElementById('project-list');
             data.forEach(repo => {
                 const listItem = document.createElement('li');
                 listItem.className = 'list-group-item hvr-grow';
-                listItem.innerHTML = `<i class="
+                listItem.innerHTML = `<i class="fas fa-code-branch"></i> <a href="${repo.html_url}" target="_blank">${
+
