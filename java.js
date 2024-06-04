ee1b2f23a0bde7958a87e95c36b04ef6951cf554
YashSoPro <105553471+YashSoPro@users.noreply.github.com>
2024-06-04 11:47:28 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,52 +1,42 @@
 $(document).ready(function(){
+    // Profile information
+    $("#name").typewriter({
+        text: "Yash Kumar",
+        waitingTime: 1000,
+        delay: 150,
+        hide: 0,
+        cursor: true,
+    });
+    
+    $("#alias").typewriter({
+        text: "Also known as Sable Meow",
+        waitingTime: 2000,
+        delay: 150,
+        hide: 0,
+        cursor: true,
+    });
+    
+    $("#hobbies").typewriter({
+        text: "Music and I.T",
+        waitingTime: 3000,
+        delay: 150,
+        hide: 0,
+        cursor: true,
+    });
+
+    // Fetch GitHub projects and display them
     const projects = [
         { name: "emuinabox", link: "https://github.com/YashSoPro/emuinabox" },
         { name: "MeowCraftServer", link: "https://github.com/YashSoPro/MeowCraftServer" },
         { name: "meowmc", link: "https://github.com/YashSoPro/meowmc" }
     ];
     
-    // Function to display projects
-    function displayProjects() {
-        const projectContainer = $("#projects");
-        projects.forEach(project => {
-            const listItem = `
-                <li class="project-box hvr-grow-shadow animate__animated animate__fadeInUp">
-                    <a href="${project.link}" target="_blank" class="project-link">
-                        <img src="https://i.ibb.co/kBCbD6y/github-icon.png" alt="GitHub">
-                        <p>${project.name}</p>
-                    </a>
-                </li>`;
-            projectContainer.append(listItem);
-        });
-    }
-    
-    displayProjects();
-    
-    // Initialize typewriter effects with delays
-    setTimeout(function() {
-        $("#name").typewriter({
-            delay: 150,
-            waitingTime: 1000,
-            hide: 0,
-            cursor: true
-        });
-    }, 1000);
-    
-    setTimeout(function() {
-        $("#alias").typewriter({
-            delay: 150,
-            waitingTime: 2000,
-            hide: 0,
-            cursor: true
-        });
-    }, 8000);
-    
-    setTimeout(function() {
-        $("#hobbies").typewriter({
-            delay: 150,
-            waitingTime: 3000,
-            hide: 0,
-            cursor: true
-        });
-    }, 15000);
+    const projectContainer = $("#projects");
+    projects.forEach(project => {
+        const listItem = `
+            <li class="project-box">
+                <a href="${project.link}" target="_blank" class="project-link">${project.name}</a>
+            </li>`;
+        projectContainer.append(listItem);
+    });
 });
