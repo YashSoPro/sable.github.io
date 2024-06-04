0a0247c0299f4ac8167c9cceadafb5cc28b73cf2
YashSoPro <105553471+YashSoPro@users.noreply.github.com>
2024-06-04 18:43:41 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // GitHub repositories fetching
+    // Toggle dark mode function
+    const darkModeBtn = document.getElementById('dark-mode-btn');
+    const body = document.body;
+
+    darkModeBtn.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+
+        // Toggle dark mode image visibility
+        const darkModeImage = document.getElementById('dark-mode-image');
+        darkModeImage.style.display = body.classList.contains('dark-mode') ? 'flex' : 'none';
+    });
+
+    // Fetch GitHub repositories
     fetch('https://api.github.com/users/YashSoPro/repos')
         .then(response => response.json())
         .then(data => {
@@ -7,5 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
             data.forEach(repo => {
                 const listItem = document.createElement('li');
                 listItem.className = 'list-group-item hvr-grow';
-                listItem.innerHTML = `<i class="fas fa-code-branch"></i> <a href="${repo.html_url}" target="_blank">${
-
+                listItem.innerHTML = `<i class="fas fa-code-branch"></i> <a href="${repo.html_url}" target="_blank">${repo.name}</a>`;
+                projectList.appendChild(listItem);
+            });
+        });
+});
